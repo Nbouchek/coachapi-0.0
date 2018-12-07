207d3f1c0ff3c331955a5b31185467c97f87a34f
Nacer Bouchekhima <nbouche@gmail.com>
2018-12-07 07:19:07 +0000

    Adding Angular files

--- src/main/resources/static/client/src/app/app.module.ts
+++ src/main/resources/static/client/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {CarListComponent} from './car-list/car-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
